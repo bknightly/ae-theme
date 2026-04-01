20993336cbd32d1628c5b85bce8af0fa59ca28dd
Bill Knight <bill@knightly.io>
2026-03-31 14:47:50 -1000

    Edit: Google Analytics tracking code

--- inc/google-analytics.php
+++ inc/google-analytics.php
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VXM8PPXFWY"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DS7LG5PEH5"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-VXM8PPXFWY');
+  gtag('config', 'G-DS7LG5PEH5');
 </script>
